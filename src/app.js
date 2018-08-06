a3f0e9ca2db38753b51dd77ce020ea7f741e809a
Viktor Grom <viktorgrom84@gmail.com>
2018-08-06 15:19:39 -0700

    initial commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+    ReactDOM.render(<h2>Hello World</h2>, document.getElementById('app'));
+
+
+
+
