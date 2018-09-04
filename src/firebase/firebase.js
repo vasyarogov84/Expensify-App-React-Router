fdc4b1f1ae73945234ab91f265a5afd6a26bb728
Viktor Grom <viktorgrom84@gmail.com>
2018-09-03 18:13:36 -0700

    remove api key

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,7 @@ import * as firebase from 'firebase';
 
 
 var config = {
-    apiKey: "AIzaSyAjRruuP0bw-hD8OLn46Fxjl58i-qf11Uw",
-    authDomain: "expensify-7a50b.firebaseapp.com",
-    databaseURL: "https://expensify-7a50b.firebaseio.com",
-    projectId: "expensify-7a50b",
-    storageBucket: "expensify-7a50b.appspot.com",
-    messagingSenderId: "455394917598"
+   
 };
 firebase.initializeApp(config);
 
@@ -18,12 +13,23 @@ data.ref().set({
     age: 33,
     city: 'Portland',
     nationality: 'Russian',
-   });
-
-data.ref('attributes').set({
-    height: 5.11,
-    weight: 195
+}).then(() => {
+    console.log('Data is saved');
+}).catch((error) => {
+    console.log(error);
 });
+
+//data.ref('attributes').set({
+   // height: 5.11,
+   // weight: 195
+//}).then(() => {
+    //console.log('Attributes added');
+//}).catch((e) => {
+    //console.log(e);
+//});
+
+data.ref('age').remove();  // data.ref('age').set(null);
+data.ref().update({ city: 'Aloha', nationality: 'Aborigen'});
 
 
 
--- src/playground/promise.js
+++ src/playground/promise.js
@@ -1,9 +1,6 @@
 let promise = new Promise((res, req) => {
     res('I learn what is Promise');
 });
-
-
-
 promise.then((data) => {
     console.log(data);
 });
